91603dd1a71fec78f60677407c4757f069f26fa2
Kirill <ke.malikov@yandex.ru>
2020-10-17 20:07:30 +0400

    инпут на карточке рыбы

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,3 +1,26 @@
 $(function(){
+    let counter = $('.btn__quantity');
 
+
+    counter.each(function () {
+        let input = $(this).find(".quantity__pole")
+        let minBtn = $(this).find(".quantity__btnmin")
+        let plusBtn = $(this).find(".quantity__btnplas")
+
+        plusBtn.click(function () {
+            input.val(Number(input.val()) + 1)
+        });
+        minBtn.click(function () {
+            if(Number(input.val()) > 1 ) {
+                input.val(Number(input.val()) - 1)
+            }
+        })
+
+        function resizeInput() {
+
+        }
+
+
+
+    })
 });
--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ let path = {
   src: {
     html: [source_folder + "/*.html", "!"+ source_folder + "/_*.html"],
     css: source_folder + "/scss/*.{scss,css}",
-    js: source_folder + "/js/script.js",
+    js: source_folder + "/js/*.js",
     img: source_folder + "/img/**/*.{png,jpg,svg,gif,ico,webp}",
     fonts: source_folder + "/fonts/*.ttf"
   },
